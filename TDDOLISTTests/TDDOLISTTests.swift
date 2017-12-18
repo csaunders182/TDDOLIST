2b8fb2a753ca1e11d61db28ab6b17a7caa1dfaee
Cole Saunders <csaunders180@gmail.com>
2017-12-18 14:16:29 -0800

    Initial Commit

---
+++ TDDOLISTTests/TDDOLISTTests.swift
@@ -0,0 +1,36 @@
+//
+//  TDDOLISTTests.swift
+//  TDDOLISTTests
+//
+//  Created by Cole Saunders on 12/18/17.
+//  Copyright © 2017 Cole Saunders. All rights reserved.
+//
+
+import XCTest
+@testable import TDDOLIST
+
+class TDDOLISTTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
